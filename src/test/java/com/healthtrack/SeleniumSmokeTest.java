56ec3729a4a191e0f23118930962f25876cebef3
alfonso <alfonso.garrido12@gmail.com>
2025-06-29 15:34:46 -0400

    Ejecuta Selenium en modo headless para compatibilidad con CI

--- src/test/java/com/healthtrack/SeleniumSmokeTest.java
+++ src/test/java/com/healthtrack/SeleniumSmokeTest.java
@@ -3,6 +3,7 @@ package com.healthtrack;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumSmokeTest {
@@ -10,7 +11,14 @@ public class SeleniumSmokeTest {
     @Test
     public void testGoogleTitle() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         assert driver.getTitle().contains("Google");
         driver.quit();
